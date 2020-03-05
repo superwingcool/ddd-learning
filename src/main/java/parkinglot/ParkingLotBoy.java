683fc5dfbc656a4fa560c06a287dde1768fb39ff
superwingcool <superwingcool@gmail.com>
2020-03-05 14:40:46 +0800

    init

--- src/main/java/parkinglot/ParkingLotBoy.java
+++ src/main/java/parkinglot/ParkingLotBoy.java
@@ -18,38 +18,6 @@ public class ParkingLotBoy {
         parkingLots.add(parkingLot2);
     }
 
-    public static void main(String[] args) {
-
-        ParkingLotBoy parkingLotBoy = new ParkingLotBoy();
-
-        Car car1 = new Car("a123456");
-        Car car2 = new Car("b123456");
-        Car car3 = new Car("c123456");
-        Car car4 = new Car("d123456");
-        Car car5 = new Car("e123456");
-        Car car6 = new Car("f123456");
-
-        Ticket t1 = parkingLotBoy.parking(car1);
-        System.out.println(t1.toString());
-
-        Ticket t2 = parkingLotBoy.parking(car2);
-        System.out.println(t2.toString());
-
-        Ticket t3 = parkingLotBoy.parking(car3);
-        System.out.println(t3.toString());
-
-        Ticket t4 = parkingLotBoy.parking(car4);
-        System.out.println(t4.toString());
-
-        Ticket t5 = parkingLotBoy.parking(car5);
-        System.out.println(t5.toString());
-
-        Car car = parkingLotBoy.getCar(t1);
-        System.out.println(car.getPlateNumber());
-
-        Ticket t6 = parkingLotBoy.parking(car6);
-        System.out.println(t6.toString());
-    }
 
     public Ticket parking(Car car) {
         return getValidateParkingLot().getTicket(car);
